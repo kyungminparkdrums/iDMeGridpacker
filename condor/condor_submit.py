909e96c43a91f40eebacd54e773ac1ac8602586a
Kyungmin Park <kyungmin.park@cern.ch>
2023-09-28 11:54:41 -0500

    condor submission

---
+++ condor/condor_submit.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--m1', type=float, help='M1', required=True)
+parser.add_argument('--delta', type=float, help='Mass splitting', required=True)
+parser.add_argument('--onejet', type=str, help='1jet or not, default is False', default='False')
+parser.add_argument('--mA_over_m1', type=float, help="M(A')/M1, default is 3", default=3.)
+
+args = parser.parse_args()
+
+m1 = args.m1
+delta = args.delta
+onejet = args.onejet
+mA_over_m1 = args.mA_over_m1
+
+arg_cmd = f'Arguments = {m1} {delta} {onejet} {mA_over_m1}'
+
+currentDir = os.getcwd()
+
+os.chdir('../../../../')
+os.system('tar -czf submit.tar.gz *')
+
+os.chdir(currentDir)
+os.system('mv ../../../../submit.tar.gz .')
+
+logDir = f'log_m1_{m1}_dMchi_{delta}_onejet_{onejet}_mA_over_m1_{mA_over_m1}'
+if not os.path.isdir(logDir):
+    os.makedirs(logDir)
+
+condor_cmd = 'condor_submit condorTemplate.jdl'
+condor_cmd += f' -append \"{arg_cmd}\"'
+condor_cmd += ' -append \"transfer_input_files = submit.tar.gz\"'
+condor_cmd += ' -append \"output = {logDir}/\$(Cluster)_\$(Process).out\"'
+condor_cmd += ' -append \"error = {logDir}/\$(Cluster)_\$(Process).err\"'
+condor_cmd += ' -append \"log = {logDir}/\$(Cluster)_\$(Process).log\"'
+condor_cmd += ' -append \"Queue 1\"'
+
+print(condor_cmd)
+os.system(condor_cmd)
---
+++ condor/gridpack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+m1=$1
+delta=$2
+onejet=$3
+mA_over_m1=$4
+
+mkdir gridpacks
+cd gridpacks
+
+tar xf ../submit.tar.gz
+
+cd iDMeGridpacker
+
+python makeGridpack_electrons.py --m1 $m1 --delta $delta --onejet $onejet --mA_over_m1 $mA_over_m1
+
+cd ..
+ls
+
+outfile=$(ls | grep ".tar.xz")
+
+echo outfile
+
+xrdcp $outfile root://cmseos.fnal.gov//store/group/lpcmetx/iDMe/gridpacks/
